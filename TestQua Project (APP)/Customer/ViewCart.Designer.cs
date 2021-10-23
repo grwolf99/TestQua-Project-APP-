d78680e644a8192a963eb1f760cf4962878da38b
GRWolfy <67980159+GRWolfy@users.noreply.github.com>
2021-10-23 16:26:24 +0800

    View Cart

--- TestQua Project (APP)/Customer/ViewCart.Designer.cs
+++ TestQua Project (APP)/Customer/ViewCart.Designer.cs
@@ -33,17 +33,29 @@ namespace TestQua_Project__APP_.Customer
          this.btnBack = new System.Windows.Forms.Button();
          this.btnCheckout = new System.Windows.Forms.Button();
          this.btnDelete = new System.Windows.Forms.Button();
-         this.button1 = new System.Windows.Forms.Button();
+         this.btnUpdate = new System.Windows.Forms.Button();
+         this.lblProductName = new System.Windows.Forms.Label();
+         this.txtName = new System.Windows.Forms.TextBox();
+         this.txtTotalPrice = new System.Windows.Forms.TextBox();
+         this.asd = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.numericUpDown_Quantity = new System.Windows.Forms.NumericUpDown();
+         this.pictureBox = new System.Windows.Forms.PictureBox();
+         this.label4 = new System.Windows.Forms.Label();
+         this.lblTotal = new System.Windows.Forms.Label();
          ((System.ComponentModel.ISupportInitialize)(this.datagridViewCart)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Quantity)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
          this.SuspendLayout();
          // 
          // datagridViewCart
          // 
          this.datagridViewCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-         this.datagridViewCart.Location = new System.Drawing.Point(93, 64);
+         this.datagridViewCart.Location = new System.Drawing.Point(58, 84);
          this.datagridViewCart.Name = "datagridViewCart";
-         this.datagridViewCart.Size = new System.Drawing.Size(562, 282);
+         this.datagridViewCart.Size = new System.Drawing.Size(634, 361);
          this.datagridViewCart.TabIndex = 0;
+         this.datagridViewCart.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridViewCart_CellContentClick);
          // 
          // btnBack
          // 
@@ -57,9 +69,10 @@ namespace TestQua_Project__APP_.Customer
          // 
          // btnCheckout
          // 
-         this.btnCheckout.Location = new System.Drawing.Point(284, 590);
+         this.btnCheckout.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnCheckout.Location = new System.Drawing.Point(390, 567);
          this.btnCheckout.Name = "btnCheckout";
-         this.btnCheckout.Size = new System.Drawing.Size(75, 23);
+         this.btnCheckout.Size = new System.Drawing.Size(215, 49);
          this.btnCheckout.TabIndex = 2;
          this.btnCheckout.Text = "Checkout";
          this.btnCheckout.UseVisualStyleBackColor = true;
@@ -67,21 +80,108 @@ namespace TestQua_Project__APP_.Customer
          // 
          // btnDelete
          // 
-         this.btnDelete.Location = new System.Drawing.Point(684, 429);
+         this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnDelete.Location = new System.Drawing.Point(519, 481);
          this.btnDelete.Name = "btnDelete";
-         this.btnDelete.Size = new System.Drawing.Size(75, 23);
+         this.btnDelete.Size = new System.Drawing.Size(136, 47);
          this.btnDelete.TabIndex = 3;
          this.btnDelete.Text = "Delete";
          this.btnDelete.UseVisualStyleBackColor = true;
+         this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
          // 
-         // button1
+         // btnUpdate
          // 
-         this.button1.Location = new System.Drawing.Point(579, 428);
-         this.button1.Name = "button1";
-         this.button1.Size = new System.Drawing.Size(75, 23);
-         this.button1.TabIndex = 4;
-         this.button1.Text = "button1";
-         this.button1.UseVisualStyleBackColor = true;
+         this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.btnUpdate.Location = new System.Drawing.Point(324, 481);
+         this.btnUpdate.Name = "btnUpdate";
+         this.btnUpdate.Size = new System.Drawing.Size(136, 47);
+         this.btnUpdate.TabIndex = 4;
+         this.btnUpdate.Text = "Update";
+         this.btnUpdate.UseVisualStyleBackColor = true;
+         // 
+         // lblProductName
+         // 
+         this.lblProductName.AutoSize = true;
+         this.lblProductName.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.lblProductName.Location = new System.Drawing.Point(784, 332);
+         this.lblProductName.Name = "lblProductName";
+         this.lblProductName.Size = new System.Drawing.Size(92, 36);
+         this.lblProductName.TabIndex = 5;
+         this.lblProductName.Text = "Name";
+         // 
+         // txtName
+         // 
+         this.txtName.Enabled = false;
+         this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtName.Location = new System.Drawing.Point(790, 384);
+         this.txtName.Name = "txtName";
+         this.txtName.Size = new System.Drawing.Size(229, 41);
+         this.txtName.TabIndex = 6;
+         // 
+         // txtTotalPrice
+         // 
+         this.txtTotalPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.txtTotalPrice.Location = new System.Drawing.Point(790, 601);
+         this.txtTotalPrice.Name = "txtTotalPrice";
+         this.txtTotalPrice.Size = new System.Drawing.Size(229, 41);
+         this.txtTotalPrice.TabIndex = 8;
+         // 
+         // asd
+         // 
+         this.asd.AutoSize = true;
+         this.asd.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.asd.Location = new System.Drawing.Point(784, 560);
+         this.asd.Name = "asd";
+         this.asd.Size = new System.Drawing.Size(83, 36);
+         this.asd.TabIndex = 7;
+         this.asd.Text = "Price";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label3.Location = new System.Drawing.Point(784, 454);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(125, 36);
+         this.label3.TabIndex = 9;
+         this.label3.Text = "Quantity";
+         // 
+         // numericUpDown_Quantity
+         // 
+         this.numericUpDown_Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.numericUpDown_Quantity.Location = new System.Drawing.Point(790, 503);
+         this.numericUpDown_Quantity.Name = "numericUpDown_Quantity";
+         this.numericUpDown_Quantity.Size = new System.Drawing.Size(249, 41);
+         this.numericUpDown_Quantity.TabIndex = 11;
+         this.numericUpDown_Quantity.ValueChanged += new System.EventHandler(this.numericUpDown_Quantity_ValueChanged);
+         // 
+         // pictureBox
+         // 
+         this.pictureBox.Location = new System.Drawing.Point(750, 33);
+         this.pictureBox.Name = "pictureBox";
+         this.pictureBox.Size = new System.Drawing.Size(297, 278);
+         this.pictureBox.TabIndex = 12;
+         this.pictureBox.TabStop = false;
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label4.Location = new System.Drawing.Point(14, 573);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(89, 36);
+         this.label4.TabIndex = 13;
+         this.label4.Text = "Total:";
+         // 
+         // lblTotal
+         // 
+         this.lblTotal.AutoSize = true;
+         this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.lblTotal.Location = new System.Drawing.Point(98, 573);
+         this.lblTotal.Name = "lblTotal";
+         this.lblTotal.Size = new System.Drawing.Size(115, 36);
+         this.lblTotal.TabIndex = 14;
+         this.lblTotal.Text = "number";
          // 
          // ViewCart
          // 
@@ -89,7 +189,16 @@ namespace TestQua_Project__APP_.Customer
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(7)))), ((int)(((byte)(48)))));
          this.ClientSize = new System.Drawing.Size(1087, 664);
-         this.Controls.Add(this.button1);
+         this.Controls.Add(this.lblTotal);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.pictureBox);
+         this.Controls.Add(this.numericUpDown_Quantity);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.txtTotalPrice);
+         this.Controls.Add(this.asd);
+         this.Controls.Add(this.txtName);
+         this.Controls.Add(this.lblProductName);
+         this.Controls.Add(this.btnUpdate);
          this.Controls.Add(this.btnDelete);
          this.Controls.Add(this.btnCheckout);
          this.Controls.Add(this.btnBack);
@@ -100,7 +209,10 @@ namespace TestQua_Project__APP_.Customer
          this.Text = "ViewCart";
          this.Load += new System.EventHandler(this.ViewCart_Load);
          ((System.ComponentModel.ISupportInitialize)(this.datagridViewCart)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Quantity)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
          this.ResumeLayout(false);
+         this.PerformLayout();
 
       }
 
@@ -110,6 +222,15 @@ namespace TestQua_Project__APP_.Customer
       private System.Windows.Forms.Button btnBack;
       private System.Windows.Forms.Button btnCheckout;
       private System.Windows.Forms.Button btnDelete;
-      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.Button btnUpdate;
+      private System.Windows.Forms.Label lblProductName;
+      private System.Windows.Forms.TextBox txtName;
+      private System.Windows.Forms.TextBox txtTotalPrice;
+      private System.Windows.Forms.Label asd;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.NumericUpDown numericUpDown_Quantity;
+      private System.Windows.Forms.PictureBox pictureBox;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label lblTotal;
    }
 }
--- TestQua Project (APP)/Customer/ViewCart.cs
+++ TestQua Project (APP)/Customer/ViewCart.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace TestQua_Project__APP_.Customer
 {
    public partial class ViewCart : Form
    {
+      private double price = 0;
+      private double totalprice = 0;
+      private int productid  = 0;
+      private int setMax = 0;
       public ViewCart()
       {
          InitializeComponent();
@@ -19,7 +19,14 @@ namespace TestQua_Project__APP_.Customer
 
       private void ViewCart_Load(object sender, EventArgs e)
       {
-         viewCart();   
+         viewCart();
+         setTotalPrice();
+      }
+
+      private void setNumericUpandDown()
+      {
+         numericUpDown_Quantity.Minimum = 1;
+         numericUpDown_Quantity.Maximum = setMax;
       }
 
       private void viewCart()
@@ -41,6 +48,90 @@ namespace TestQua_Project__APP_.Customer
          var customercheckout = new CustomerCheckout();
          customercheckout.Show();
          Close();
+      }
+
+      private void datagridViewCart_CellContentClick(object sender, DataGridViewCellEventArgs e)
+      {
+         txtName.Text = datagridViewCart.Rows[e.RowIndex].Cells["productname"].Value.ToString();
+         price =  Convert.ToDouble(datagridViewCart.Rows[e.RowIndex].Cells["productprice"].Value);
+         double quantity =  Convert.ToDouble(datagridViewCart.Rows[e.RowIndex].Cells["quantity"].Value);
+         txtTotalPrice.Text = (quantity * price).ToString();
+         productid = Convert.ToInt32(datagridViewCart.Rows[e.RowIndex].Cells["productid"].Value);
+         numericUpDown_Quantity.Value = Convert.ToDecimal(quantity);
+         setMax = Convert.ToInt32(datagridViewCart.Rows[e.RowIndex].Cells["quantity1"].Value);
+         byte[] img = (byte[])(datagridViewCart.Rows[e.RowIndex].Cells["productimage"].Value);
+         setNumericUpandDown();
+
+         if (img == null)
+         {
+            pictureBox.Image = null;
+         }
+         else
+         {
+            MemoryStream ms = new MemoryStream(img);
+            pictureBox.Image = Image.FromStream(ms);
+            pictureBox.BackgroundImageLayout = ImageLayout.Stretch;
+         }
+      }
+
+      private void numericUpDown_Quantity_ValueChanged(object sender, EventArgs e)
+      {
+         txtTotalPrice.Text = (Convert.ToDouble(numericUpDown_Quantity.Value) * price).ToString();
+      }
+
+      private void clearFields()
+      {
+         pictureBox.Dispose();
+         txtName.Clear();
+         txtTotalPrice.Clear();
+         numericUpDown_Quantity.Value = 0;
+      }
+
+      private void btnDelete_Click(object sender, EventArgs e)
+      {
+         try
+         {
+            Connection.DB();
+            var gen = MessageBox.Show("Remove to cart?", "Delete record", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (gen == DialogResult.Yes)
+            {
+               Function.gen = "DELETE FROM CartDB WHERE UserId = '" + productid + "' ";
+               Function.command = new SqlCommand(Function.gen, Connection.con);
+               Function.command.ExecuteNonQuery();
+               Connection.con.Close();
+               clearFields();
+            }
+         }
+
+         catch (Exception ex)
+         {
+            MessageBox.Show(ex.Message);
+         }
+      }
+
+      private void setTotalPrice() 
+      {
+         try
+         {
+            Connection.DB();
+            Function.gen = "SELECT * FROM CartDB WHERE userid = '"+ Login.userid +"' ";
+            Function.command = new SqlCommand(Function.gen, Connection.con);
+            Function.reader = Function.command.ExecuteReader();
+
+            if (Function.reader.HasRows)
+            {
+               Function.reader.Read();
+               totalprice += Convert.ToDouble(Function.reader["productprice"]) * Convert.ToDouble(Function.reader["quantity"]);
+               lblTotal.Text = "P" + totalprice.ToString();
+            }
+         }
+
+         catch (Exception ex)
+         {
+            Connection.con.Close();
+            MessageBox.Show(ex.Message);
+         }
       }
    }
 }
